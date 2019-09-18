c95bf421f0d3a11ad4f0c8dddaec65563a99a5dd
ilabstaff <ilabstaff@172.16.116.128>
2019-09-17 19:17:15 -0700

    NO

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,11 +9,14 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> dualStack = new Stack<Double>();
+		Random ran = new Random();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for (int i = 0; i < 100; i++) {
+			dualStack.push(ran.nextDouble()*100);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		String s = JOptionPane.showInputDialog("Enter 0-100");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
 		
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,12 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.*;
+
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,6 +19,48 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	
+	Stack<Character> undo = new Stack<Character>();
+	JFrame Frame;
+	JPanel Panel;
+	JLabel Label;
+	String text;
+	_02_TextUndoRedo(){
+		Frame = new JFrame();
+		Panel = new JPanel();
+		Label = new JLabel();
+		Frame.add(Panel);
+		Panel.add(Label);
+		Frame.addKeyListener(this);
+		text = "";
+		Frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		_02_TextUndoRedo c = new _02_TextUndoRedo();
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	
+		if (e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
+			
+		}
+	text+=e.getKeyChar();
+	undo.push(e.getKeyChar());
+	Label.setText(text);
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
