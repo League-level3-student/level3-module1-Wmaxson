5e3aa3f4d3fabe936a17a380bbe83eb723a62085
ilabstaff <ilabstaff@172.16.116.77>
2019-10-01 19:12:48 -0700

    got a 1/4 of they way through HangMan

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,6 +1,51 @@
 package _04_HangMan;
 
+import java.util.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
+
+
 public class HangMan{
+	
+	
+	
+	int lives;
+	
+	String rounds;
+	
+	Stack<String> words;
+	
+	int round;
+	
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	
+	
+	public HangMan(){
+		
+		rounds = JOptionPane.showInputDialog("Welcome to HANGMAN. "+ "\nHow many words would you like to guess");
+		
+		round = Integer.parseInt(rounds);
+		
+		for (int i = 0; i < round; i++) {
+			
+			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+		
+		}
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 	
 	
 	
