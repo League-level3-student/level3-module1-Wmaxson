a52aa7eaa5c68dd1dd6137212e96724ddf87bb60
ilabstaff <ilabstaff@172.16.116.128>
2019-10-08 19:19:55 -0700

    likjwERKDBFSVOPRIDFKO[JLNCKOSJFIDKOJLBFDO;JLNKMKDWEOJHKBLRSV;JOGLBKVNLS;OKIRHUDKJLSV;OK;JBLNV;

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -21,30 +21,45 @@ public class HangMan{
 	Stack<String> words;
 	
 	int round;
-	
+	String currentWord;
+	String displayWord;
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
 	
 	
 	public HangMan(){
-		
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+		frame.add(panel);
+		panel.add(label);
+		frame.setVisible(true);
 		rounds = JOptionPane.showInputDialog("Welcome to HANGMAN. "+ "\nHow many words would you like to guess");
 		
 		round = Integer.parseInt(rounds);
-		
+		words = new Stack();
 		for (int i = 0; i < round; i++) {
 			
 			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
 		
 		}
+		displayWord = "";
+		currentWord = words.get(words.size()-1);
 		
+		words.pop();
+		
+		for (int i = 0; i < currentWord.length(); i++) {
+			 displayWord+="_";
+		}
+		label.setText(displayWord);
+		frame.pack();
 		
 		
 	}
 	
 	public static void main(String[] args) {
-		
+		new HangMan();
 	}
 	
 	
