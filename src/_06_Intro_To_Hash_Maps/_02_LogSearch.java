91aa01f7a8e13b08eeb756fa2b2013d119d9f472
ilabstaff <ilabstaff@172.16.116.128>
2019-10-29 19:19:16 -0700

    Finished log Search

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,14 @@
 package _06_Intro_To_Hash_Maps;
 
 
-public class _02_LogSearch {
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+import javax.swing.*;
+
+public class _02_LogSearch implements ActionListener {
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -30,6 +37,78 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
-	
-	
+	HashMap<Integer, String> map;
+	JFrame frame;
+	JPanel panel;
+	JButton addEntry;
+	JButton SearchID;
+	JButton Remove;
+	JButton View;
+	 
+	 _02_LogSearch() {
+		 frame = new JFrame();
+		 frame.setVisible(true);
+		 panel = new JPanel();
+		 
+		 addEntry = new JButton("Add entry");
+		 
+		 SearchID = new JButton("Search ID");
+		 
+		 Remove = new JButton("Remove");
+		 
+		 View = new JButton("View");
+		 
+		
+		 map = new HashMap<Integer, String>();
+		 frame.add(panel);
+		 panel.add(addEntry);
+		 panel.add(SearchID);
+		 panel.add(Remove);
+		 panel.add(View);
+		 frame.pack();
+		 ;
+		 addEntry.addActionListener(this);
+		 SearchID.addActionListener(this);
+		 Remove.addActionListener(this);
+		 View.addActionListener(this);
+	 }
+	 public static void main(String[] args) {
+		new _02_LogSearch();
+	}
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == addEntry) {
+			String tempName = "";
+			String tempID = "";                  						                                     
+			int tempIDInt;                                                                        
+			      ;                                                                                                        
+			tempName = JOptionPane.showInputDialog("Type in the Name below");
+			tempID   = JOptionPane.showInputDialog("Type in the ID below");
+			tempIDInt = Integer.parseInt(tempID);
+			map.put(tempIDInt, tempName);
+			
+		} else if(e.getSource() == SearchID) {
+			String tempID = "";
+			int tempIDInt = 0;
+			tempID = JOptionPane.showInputDialog("Type in ID below.");
+			tempIDInt = Integer.parseInt(tempID);
+			JOptionPane.showMessageDialog(null, map.get(tempIDInt));
+		} else if(e.getSource() == Remove) {
+			String tempID = "";
+			int tempIDInt = 0;
+			tempID = JOptionPane.showInputDialog("Type in ID below.");
+			tempIDInt = Integer.parseInt(tempID);
+			map.remove(tempIDInt);
+		} else if(!!!!(e.getSource() == View)) {
+			String message;
+			message = "";
+			for(Integer key : map.keySet()) {
+				String v = map.get(key);
+				message += "\n" + v+ ", " + key;
+			};
+			JOptionPane.showMessageDialog(null, message);
+			
+			
+		}
+	}
 }
